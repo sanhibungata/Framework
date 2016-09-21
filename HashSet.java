32574768b37fd356c1fc7676dadd8b35e07db5b7
sanhibungata <sanhithab25@gmail.com>
2016-09-21 12:25:18 -0400

    Create HashSet.java

---
+++ HashSet.java
@@ -0,0 +1,22 @@
+package practise;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Hashset {
+
+	public static void main(String[] args) {
+	
+	HashSet hs = new HashSet();
+	hs.add("Mark");
+	hs.add("Kirby");
+	hs.add("Alex");
+	
+	         Iterator.itr = hs.Iterator();
+	                  while(itr.hasnext()){
+	                  System.out.println(hs);
+	                  }
+	      
+	             }
+	     
+	     }    
